e86c242bfbb4c42b3225caf948970cf362895eae
richardcsuwandi <59959022+richardcsuwandi@users.noreply.github.com>
2020-06-25 14:57:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     st.subheader("By Richard Cornelius Suwandi")
     st.sidebar.markdown("By Richard Cornelius Suwandi")
     st.markdown(
-        "Time series visualizations on the number of confirmed cases,\
+        "Time series visualizations on the number of confirmed cases, \
         deaths, and recoveries from COVID-19. \
         between 22 January 2020 to 22 June 2020."
         )
@@ -22,14 +22,17 @@ def main():
         data.drop(["Lat", "Long"], axis=1, inplace=True)
         return data
 
+    # Load the datasets
     covid_confirmed = load_data("covid_confirmed.csv")
     covid_deaths = load_data("covid_deaths.csv")
     covid_recovered = load_data("covid_confirmed.csv")
 
+    # Aggregate by country/region
     covid_confirmed_agg = covid_confirmed.groupby("Country/Region").sum()
     covid_deaths_agg = covid_deaths.groupby("Country/Region").sum()
     covid_recovered_agg = covid_recovered.groupby("Country/Region").sum()
 
+    # Dictionary to store the datasets and labels
     covid_data_dict = {
         "Confirmed Cases": [covid_confirmed_agg, "Infection Rate"] ,
         "Death Cases": [covid_deaths_agg, "Death Rate"],
@@ -39,12 +42,13 @@ def main():
     choice = st.sidebar.radio("Which data to plot?", ("Confirmed Cases", "Death Cases", "Recovered Cases"))
     covid_data = covid_data_dict[choice][0]
 
-    # Plot by country/countries
+    # Country selection
     countries = st.sidebar.multiselect("Choose the country/region to plot", tuple(covid_data.index))
 
-    # Plot by month
+    # Month selection
     month = st.sidebar.radio("Choose a specific month to plot", ("January", "February",  "March", "April", "May", "June", "All Months"))
 
+    # Dictionary to store months and the corresponding start-end dates
     months_dict = {
         "January": (0, 10),
         "February": (10, 39),
@@ -93,6 +97,7 @@ def main():
             plt.legend()
             st.pyplot()
 
+    # Show raw data
     if st.sidebar.checkbox("Show raw data", False):
         st.subheader(f"COVID-19 Data for {len(covid_data)} Countries/Regions")
         st.write(covid_data)
